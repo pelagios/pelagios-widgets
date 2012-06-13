db160f247dac36e1622c6375ba9e1a5b41366bf7
Juliette Culver <jvvw@jvvw.com>
2012-06-13 10:19:39 +0100

    Issue #48 Add manual test cases

--- demo/place/pelagios_include.php
+++
@@ -1,4 +0,0 @@
- <script type="text/javascript" src="http://<?php  echo $_SERVER['SERVER_NAME']?>/widgets/build/lib/require.js">
-</script>
- <script src="http://<?php  echo $_SERVER['SERVER_NAME']?>/widgets/build/place.js">
-</script>
