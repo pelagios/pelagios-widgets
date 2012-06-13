bdf0e35af70cab3e5d478c9d3c881d7b7b657a0d
Juliette Culver <jvvw@jvvw.com>
2012-06-13 15:34:56 +0100

    Issue #50 Temporarily removing the hit text

--- build/app/dataset.js
+++
@@ -1,8 +0,0 @@
-/**
- * Pelagios dataset library - will move into a json file eventually, 
- * however need to think about how to handle the async loading of the json 
- * first
- * @license GPL v3(see LICENSE.txt)
- */
-
-define([],function(){function getDatasetStrapline(dataset){var straplines={Arachne:"A web research tool of the German Archaeological Institute for searching mass archaeological records","Google Ancient Places":"A Geographic Annotation Platform for reading texts spatially",nomisma:"Representing ancient coins online",SPQR:"From Roman inscriptions to resource descriptions","Regnum Francorum Online":"Interactive Maps and Sources of Late Antique and Early Medieval Europe",Perseus:"Annotated occurrences of place entities in texts from the Perseus Digital Library",CLAROS:"Aggregated search of art and archaeology data from the classical world and the far east across four millennia","FASTI Online":"A database of archaeological excavations since the year 2000",flickr:"Photo sharing website"},strapline=straplines[dataset]?straplines[dataset]:"";return strapline}function getDatasetIconURL(dataset){var icons={CLAROS:"CLAROS.png","FASTI Online":"FASTI.png","Google Ancient Places":"gap_logo.png",Arachne:"arachne.png","Open Context":"open-context.png",SPQR:"SPQR.png","Ure Museum":"URE.png",nomisma:"Greek.jpeg",Perseus:"Perseus.png","Regnum Francorum Online":"Regnum.png"},icon=icons[dataset]?icons[dataset]:"pelagios.png";return icon}function hasDatasetMeaningfulLabel(dataset){var meaningful=!1;switch(dataset){case"Google Ancient Places":meaningful=!0}return meaningful}return{getDatasetStrapline:getDatasetStrapline,getDatasetIconURL:getDatasetIconURL,hasDatasetMeaningfulLabel:hasDatasetMeaningfulLabel}})
