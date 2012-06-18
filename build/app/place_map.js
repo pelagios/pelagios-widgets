1eb993e21ac1a903ca8f20de286f9df6e5ecc140
Juliette Culver <jvvw@jvvw.com>
2012-06-18 16:36:03 +0100

    Ran rake

--- build/app/place_map.js
+++
@@ -1,6 +0,0 @@
-/**
- * Pelagios place map library
- * @license GPL v3(see LICENSE.txt)
- */
-
-define(["lib/async!http://maps.google.com/maps/api/js?sensor=false"],function(){function PlaceMap(id){if(!document.getElementById(id))throw Error("ERROR: Invalid ID for place map");var map=new google.maps.Map(document.getElementById(id),{zoom:8,mapTypeId:google.maps.MapTypeId.TERRAIN}),marker=new google.maps.Marker({map:map});this.refresh=function(){google.maps.event.trigger(map,"resize"),map.setCenter(marker.getPosition())},this.setMarker=function(coordinates,title){var location=new google.maps.LatLng(coordinates[1],coordinates[0]);marker.setPosition(location),marker.setTitle(title),map.setCenter(location),google.maps.event.trigger(map,"resize")}}return{PlaceMap:PlaceMap}})
--- build/app/search_map.js
+++
@@ -1,6 +0,0 @@
-/**
- * Pelagios search map library
- * @license GPL v3(see LICENSE.txt)
- */
-
-define(["lib/async!http://maps.google.com/maps/api/js?sensor=false"],function(){function SearchMap(id){if(!document.getElementById(id))throw Error("ERROR: Invalid ID for search map");var map=new google.maps.Map(document.getElementById(id),{mapTypeId:google.maps.MapTypeId.TERRAIN}),markerBounds=new google.maps.LatLngBounds,infoWindow=new google.maps.InfoWindow;this.refresh=function(){google.maps.event.trigger(map,"resize"),map.fitBounds(markerBounds)},this.addMarker=function(geojson,title,content,onClickHander){if(geojson.hasOwnProperty("geometry")&&geojson.geometry!=null&&geojson.geometry.hasOwnProperty("type")&&geojson.geometry.type=="Point"){var location=new google.maps.LatLng(geojson.geometry.coordinates[1],geojson.geometry.coordinates[0]),marker=new google.maps.Marker({position:location,map:map,title:title});markerBounds.extend(location),google.maps.event.addListener(marker,"click",function(){return function(){infoWindow.close(),infoWindow.setContent(content),infoWindow.open(map,marker),onClickHander()}}())}}}return{SearchMap:SearchMap}})
