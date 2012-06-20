a84ac98aa2f7153b337c62a736e4bcae8e1a3195
Juliette Culver <jvvw@jvvw.com>
2012-06-20 14:11:16 +0100

    Ran rake

--- build/test/examples/openlearn/openlearn_demo/olympics/cookies.js
+++
@@ -1,9 +0,0 @@
-/*******************************************************
-COOKIE FUNCTIONALITY
-Based on "Night of the Living Cookie" by Bill Dortch
-(c) 2003, Ryan Parman
-http://www.skyzyx.com
-Distributed according to SkyGPL 2.1, http://www.skyzyx.com/license/
-*******************************************************/
-
-function cookie(name,value,expires,path,domain,secure){this.name=name,this.value=value,this.expires=expires,this.path=path,this.domain=domain,this.secure=secure,this.read=function(){var ck=document.cookie,arg=this.name+"=",alen=arg.length,clen=ck.length,i=0;while(i<clen){var j=i+alen;if(ck.substring(i,j)==arg){var endstr=ck.indexOf(";",j);return endstr==-1&&(endstr=ck.length),unescape(ck.substring(j,endstr))}i=ck.indexOf(" ",i)+1;if(i==0)break}return null},this.set=function(){expStore=this.expires,exp=new Date,base=new Date(0),skew=base.getTime(),skew>0&&exp.setTime(exp.getTime()-skew),exp.setTime(exp.getTime()+this.expires*24*60*60*1e3),this.expires=exp,document.cookie=this.name+"="+escape(this.value)+(this.expires?"; expires="+this.expires.toGMTString():"")+(this.path?"; path="+this.path:"")+(this.domain?"; domain="+this.domain:"")+(this.secure?"; secure":""),this.expires=expStore},this.kill=function(){document.cookie=this.name+"="+(this.path?"; path="+this.path:"")+(this.domain?"; domain="+this.domain:"")+"; expires=Thu, 01-Jan-70 00:00:01 GMT"},this.changeName=function(chName){this.kill(),this.name=chName,this.set()},this.changeVal=function(chVal){this.kill(),this.value=chVal,this.set()},this.changeExp=function(chExp){this.kill(),this.expires=chExp,this.set()},this.changePath=function(chPath){this.kill(),this.path=chPath,this.set()},this.changeDomain=function(chDom){this.kill(),this.domain=chDom,this.set()},this.changeSecurity=function(chSec){this.kill(),this.secure=chSec,this.set()}}
