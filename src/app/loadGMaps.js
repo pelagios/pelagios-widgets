1ea817a3ab2ac17e7135efa78202aae181adaf1a
Juliette Culver <jvvw@jvvw.com>
2012-06-25 11:56:28 +0100

    Issue #84

---
+++ src/app/loadGMaps.js
@@ -0,0 +1,25 @@
+// Require.js plugin to conditionally load a library only if Google Maps v2 is 
+// not present
+define(function(){
+
+    var _noGMaps2;
+
+    function noGMaps2(){        
+        if (_noGMaps2 != null) return _noGMaps2;
+        //check only once..
+        _noGMaps2 = (typeof GMap2 == 'undefined');
+    }
+
+    return {
+        load : function(name, req, onLoad, config){
+            if(config.isBuild || noGMaps2()){
+                //don't load during build or if browser supports canvas
+                onLoad(null);
+            } else {
+                //load stuff if canvas isn't supported
+                req([name], onLoad);
+            }
+        }
+    };
+
+})
